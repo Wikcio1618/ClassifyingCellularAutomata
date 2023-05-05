7d5ad464c6acf21a78fb966691ef79a0f2bd8b03
Wikcio1618 <ciolekwiktor@gmail.com>
2023-05-05 20:01:11 +0200

    animation there

--- automaton.py
+++ automaton.py
@@ -5,10 +5,10 @@ rule_book = np.array([0 for _ in range(2**(2*radius+1))]) # initial rule is all 
 
 # Returns a 2D-array of size (num_cells, num_gens) of ones and zeros with one dim being space and the other time
 ## for given radius, rule_book and first generation
-def getEvolution(radius, rule_book, num_cells, num_gens, first_gen=None):
-	if first_gen is None:
-		first_gen=np.random.randint(2, size=num_cells)
-	return np.eye(height)
+def nextGen(num_cells, gen=None):
+	if gen is None:
+		gen=np.random.randint(2, size=num_cells)
+	return np.random.randint(2, size=num_cells)
 
 # Returns a scalar
 def getEnthropy(radius, rule_book):
--- window.py
+++ window.py
@@ -8,15 +8,16 @@ import numpy as np
 # libs for figure display
 import time
 import asyncio
+from threading import Thread, Event
 
-
+# ________________________________________ CONSTANTS _______________________________________________
 
 # Define the size of the canvas
 WIDTH = 500
 HEIGHT = 500
 
 # Define the size of each cell
-CELL_SIZE = 10
+CELL_SIZE = 6
 
 # Define the number of cells in each direction
 NUM_CELLS_X = WIDTH // CELL_SIZE # double slash works as floor division
@@ -24,22 +25,29 @@ NUM_CELLS_Y = HEIGHT // CELL_SIZE
 
 # Define the color for the cells
 CELL_COLOR = "gold"
-NO_CELL_COLOR = "green"
+NO_CELL_COLOR = "midnightblue"
 
 # Define lead colors for windows
 PRIMARY_COLOR = "gold"
 BG_COLOR = "midnightblue"
 
+# __________________________________________ VARIABLES ________________________________________________
+
+
+# _________________________________________ METHODS ________________________________________________
+
 def initWindow():
 
 	# Init window
+	global root
 	root = Tk()
 	root.geometry("900x600") # Window size
 	root.title("1D Cellular Automata")
 	root.resizable(0, 0) # No full-screen
+	root.protocol("WM_DELETE_WINDOW", on_close) # stop thread
 
 	# create top panel with widgets
-	top_frame = Frame(root, height=100)
+	top_frame = Frame(root, height=60)
 	top_frame.configure(bg=BG_COLOR)
 	ttk.Style().configure('BarWik.TCheckbutton', background=BG_COLOR, foreground=PRIMARY_COLOR)
 	# create vaiables to store checkbutton states 		
@@ -50,27 +58,35 @@ def initWindow():
 		# checkbutton is initialized and connected with a variable
 		button=ttk.Checkbutton(top_frame, style='BarWik.TCheckbutton', state='normal', variable=automaton.rule_book[i]) 
 		button.grid(column=2**(2*automaton.radius+1)-i, row=0, padx=5,pady=(20,0))
-	Button(top_frame, padx=10, pady=10, text="Run", command="").grid(column=8, row=0, pady=20, sticky=W) # to be changed for proper buttons
+	global start_stop_button
+	start_stop_button = Button(top_frame, padx=10, pady=10, text="Run", command=start_stop_thread)
+	start_stop_button.grid(column=8, row=0, pady=20, sticky=W) # to be changed for proper buttons
 	top_frame.pack(fill=BOTH, expand=True)
 
 	# create center panel with widgets
 	center_frame = Frame(root, height=500)
-	center_frame.configure(bg="red")
-	canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg="blue")
-	canvas.pack(in_=center_frame)
+	center_frame.configure(bg=BG_COLOR)
+	
+	global canvas
+	canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg=BG_COLOR, bd=1, confine=False)
+	yscrollbar = Scrollbar(root, orient="vertical", command=canvas.yview)
+	canvas.configure(yscrollcommand=yscrollbar.set)
+	global currGen	
+	currGen=np.random.randint(2, size=NUM_CELLS_X)
+	global rowToUpdate
+	rowToUpdate=0
+	global canvasUpdating
+	canvasUpdating=False
+	canvasThread = Thread(target=animateRows)
+	global thread_running_event
+	thread_running_event = Event()
+	canvasThread.start()
+	
+	canvas.pack(in_=center_frame, pady=10)
 	center_frame.pack(fill=BOTH, expand=True)
 
 	root.mainloop() # display window
 
-
-# async def runEvolution():
-# 	# TODO implement image update
-# 	while(1):
-# 		self.curr_disp[self.i,:]=self.evolution[self.i,:]
-# 		self.im.set_data(self.curr_disp)
-# 		self.canvas.draw()
-# 		self.i+=1
-# 		await asyncio.sleep(1)
 
 # function to draw a cell at a given (x, y) position
 def drawCell(x, y, is_alive):
@@ -79,13 +95,46 @@ def drawCell(x, y, is_alive):
 	x2 = x1 + CELL_SIZE
 	y2 = y1 + CELL_SIZE
 	if is_alive:
-		canvas.create_rectangle(x1, y1, x2, y2, fill=CELL_COLOR)
+		canvas.create_rectangle(x1, y1, x2, y2, fill=CELL_COLOR, outline="")
 	else:
-		canvas.create_rectangle(x1, y1, x2, y2, fill=NO_CELL_COLOR)
+		canvas.create_rectangle(x1, y1, x2, y2, fill=NO_CELL_COLOR, outline="")
 		
-def drawRow(generation: np.ndarray, row: int):
-	for i in range(NUM_CELLS_X):
-		drawCell(i, row, generation[i])
+
+def updateRow():
+	global currGen
+	global canvas
+	currGen = automaton.nextGen(NUM_CELLS_X, currGen)
+	if rowToUpdate > NUM_CELLS_Y:
+		canvas.yview_moveto(rowToUpdate-NUM_CELLS_Y)
+	for x in range(NUM_CELLS_X):
+		drawCell(x, rowToUpdate, currGen[x])
+
+	canvas.update()
 	
-def runAnimation():
-	pass
+def animateRows():
+	global thread_running_event
+	while thread_running_event.is_set():
+		updateRow()
+		global rowToUpdate
+		rowToUpdate+=1
+		time.sleep(0.01)
+	
+	
+def start_stop_thread():
+	global start_stop_button
+	global thread_running_event
+	
+	if thread_running_event.is_set():
+		thread_running_event.clear()
+		start_stop_button.config(text='Run')
+	else:
+		thread_running_event.set()
+		start_stop_button.config(text='Stop')
+		animateRows()
+		
+	
+def on_close():
+	global thread_running_event
+	thread_running_event.clear()
+	global root
+	root.destroy()
